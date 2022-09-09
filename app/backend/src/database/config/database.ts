f45d2906c7bf267e8ce9ee8d07c8b449ad2f5534
Gabriel <gabrielferrari601@gmail.com>
2022-09-09 12:32:28 -0300

    add procfile

--- app/backend/src/database/config/database.ts
+++ app/backend/src/database/config/database.ts
@@ -1,9 +1,10 @@
 import { Options } from 'sequelize';
+require('dotenv/config');
 
 const config: Options = {
   username: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASS || '123456',
-  database: 'Radarfit',
+  database: process.env.DATABASE || 'Radarfit',
   host: process.env.DB_HOST || 'localhost',
   port: Number(process.env.DB_PORT) || 5432,
   dialect: 'postgres',
